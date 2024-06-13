f882791e5e17d541919b5efa97a78c1694618e11
Dharmi Javiya <92503014+Dharmi7303@users.noreply.github.com>
2024-06-13 16:08:40 +0530

    Update CategoryController.java

--- Backend/src/main/java/com/inventorysystem/Backend/controller/CategoryController.java
+++ Backend/src/main/java/com/inventorysystem/Backend/controller/CategoryController.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+
+@RestController   
 @RequestMapping("/category")
 @CrossOrigin
 public class CategoryController {
